c95843dd4cbd599e4f0e6518e50c87410aa70c34
Daniel <babsdarhnearl@gmail.com>
2022-09-21 15:39:38 -0700

    print comb4 commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,23 +8,24 @@ int main(void)
 	int n;
 	int i;
 	int h;
-	
-	for (h= 0; h != 8 ; h++)
-	{
-		for (n = h + 1 ; n != 9; n++)
-		{
-			i = n + 1;
-			do {
-				putchar('0' + h);
-				putchar('0' + n);
-				putchar('0' + i);
-				if (h != 7)
-				{
-					putchar(',');
-					putchar(32);
+
+for (h = 0; h != 8 ; h++)
+{
+for (n = h + 1 ; n != 9; n++)
+{
+i = n + 1;
+do {
+putchar('0' + h);
+putchar('0' + n);
+putchar('0' + i);
+if (h != 7)
+{
+putchar(',');
+putchar(32);
 }
 i++;
-}while (i != 10);
+}
+while (i != 10);
 }
 }
 putchar('\n');
