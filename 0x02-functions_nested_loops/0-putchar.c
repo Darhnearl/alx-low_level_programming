ddd3f7075c97806585277526ca057cbf99089900
Daniel <babsdarhnearl@gmail.com>
2022-09-22 04:11:52 -0700

    putchar commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
  * main - prints _putchar, as a message
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 114};
-	int count, sz;
+	char dan[] = "_putchar";
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz: count++)
+	int c;
+	
+	for (c = 0; c < 8; c++)
 	{
-		_putchar(str[count]);
+		_putchar(dan[c]);
 	}
 	_putchar('\n');
 	return (0);
